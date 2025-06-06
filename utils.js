5523c951f515a8cc5e5ebe5e8e29f457dd4b912c
Andrew Feasel <andrewmail404@gmail.com>
2025-06-06 15:05:16 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,24 +1,21 @@
 "use strict";
 const $ = x => document.querySelector(x);
 const $$ = x => Array.from(document.querySelectorAll(x));
-const ParseJSON = json => {
-  try{ return {valid: false, json: JSON.parse(json)} }
-  catch { return {valid: false, json: null} }
-}
-async function * RequestGenerator(req, type) {
+async function * RequestGenerator(req, type = "json") {
   const res = await fetch(req);
   if(!res.ok){ throw new Error(res.status); }
-  switch(type) {
-    case "text": yield await res.text(); break;
-    case "json": yield await res.json(); break;
-    case "blob": yield await res.blob(); break;
-    case "bytes": yield await res.bytes(); break;
-    case "formData": yield await res.formData(); break;
-    case "arrayBuffer": yield await res.arrayBuffer(); break;
-    default: yield await res.json(); break;
-  }
+  if(typeof res[type] !== "function") { 
+    throw new Error(`Invalid type '${type}'. Expected one of: text, json, blob, bytes, arrayBuffer, formData.`);
+  } 
+  yield await res[type]();
+}
+async function BatchRequest(arr) {
+  return await Promise.all(arr.map( async ({ url, type }) => {
+    const gen = RequestGenerator(url, type);
+    return (await gen.next()).value;
+  }));
 }
 export {
   $, $$,
-  ParseJSON, RequestGenerator
+  RequestGenerator
 };
