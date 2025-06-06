a74ba880e06f447e69c8ba3bc7fe3224e1475d71
Andrew Feasel <andrewmail404@gmail.com>
2025-06-06 12:52:53 -0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,17 +1,19 @@
 "use strict";
-class Requests {
-  static async * RequestGenerator(req, type) {
-    const res = await fetch(req);
-    if(!res.ok){ throw new Error(res.status); }
-    switch(type) {
-      case "text": yield await res.text(); break;
-      case "json": yield await res.json(); break;
-      case "blob": yield await res.blob(); break;
-      case "bytes": yield await res.bytes(); break;
-      case "formData": yield await res.formData(); break;
-      case "arrayBuffer": yield await res.arrayBuffer(); break;
-      default: yield await res.json(); break;
-    }
+const $ = x => document.querySelector(x);
+async function * RequestGenerator(req, type) {
+  const res = await fetch(req);
+  if(!res.ok){ throw new Error(res.status); }
+  switch(type) {
+    case "text": yield await res.text(); break;
+    case "json": yield await res.json(); break;
+    case "blob": yield await res.blob(); break;
+    case "bytes": yield await res.bytes(); break;
+    case "formData": yield await res.formData(); break;
+    case "arrayBuffer": yield await res.arrayBuffer(); break;
+    default: yield await res.json(); break;
   }
 }
-export default Requests;
+export {
+  $,
+  RequestGenerator
+};
