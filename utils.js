7f89b6da508e0fd44a57c257d249639b47b9f51f
Andrew Feasel <andrewmail404@gmail.com>
2025-06-06 11:52:13 -0700

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,16 @@
+async function * RequestGenerator(url, type){
+	const req = await fetch(url);
+  if(!req.ok){ throw new Error(req.status); }
+  switch(type) {
+  	case "text": yield await req.text(); break;
+    case "json": yield await req.json(); break;
+    case "blob": yield await req.blob(); break;
+    case "bytes": yield await req.bytes(); break;
+    case "formData": yield await req.formData(); break;
+    case "arrayBuffer": yield await req.arrayBuffer(); break;
+    default: yield await req.json(); break;
+  }
+}
+export {
+	RequestGenerator
+}
